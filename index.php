10ca764c6c4d9c84676282f72b4b746a0c31dc39
Brayan <bhrestrepo_27@hotmail.com>
2021-12-23 16:38:38 -0500

    Update files

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HTML5</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="shortcut icon" href="media/images/icon.png" type="image/x-icon">
 </head>
+
 <body>
-    
+
+    <head></head>
+    <nav>
+        <button class="nav-button" onclick="buttonMenuClick()">Menu</button>
+        <a class="nav-bar" href="#top">Inicio</a>
+        <a class="nav-bar" href="#us">Nosotros</a>
+        <a class="nav-bar" href="#services">Servicios</a>
+        <a class="nav-bar" href="#contact">Contacto</a>
+    </nav>
+    <section></section>
+    <article></article>
+    <aside></aside>
+    <footer></footer>
 </body>
+<?php include 'utilitys/scripts.php' ?>
+
 </html>
